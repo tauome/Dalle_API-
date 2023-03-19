4b5757fdf5fd3aa06d572e5f1116e62a700e0617
Tau <tau@users-MacBook-Pro.local>
2023-03-19 09:55:14 -0400

    setup + homepage

---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import {getRandomPrompts } from '../utils'; 
+import { FormField, Loader } from '../components';
+
+
+
+export default function CreatePost() {
+  return (
+    <div>CreatePost</div>
+  )
+}
