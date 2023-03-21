171991be34de8edbc54028bac86d286c2243050b
Tau <tau@users-MacBook-Pro.local>
2023-03-21 13:45:12 -0400

    Create Post page

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 
-function FormField() {
+function FormField({LabelName, name, type, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) {
   return (
-    <div>FormField</div>
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label 
+        htmlFor={name}
+        className='block text-sm font-medium text-gray-900'
+        >
+          {LabelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+          type='button'
+          onClick={handleSurpriseMe}
+          className='font-semibold text-xs bg-[#ececf1] px-1 py-1 rounded-[5px] text-black '
+          >
+            Surprise Me
+          </button>
+        )}
+      </div>
+          <input 
+          type={type}
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+          required
+          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+          />
+    </div>
   )
 }
 
